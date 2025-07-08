083db3e1604b4193e56e7830e80559e65e1ab737
karan8891 <55722488+karan8891@users.noreply.github.com>
2025-07-08 19:10:53 +0530

    Gdw to apmf

---
+++ Event/Dags/dag1_gdw_to_apmf_push.py
@@ -0,0 +1,59 @@
+
+from airflow import models
+from airflow.providers.google.cloud.operators.cloud_storage_transfer_service import CloudDataTransferServiceCreateJobOperator
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+
+default_args = {
+    'start_date': days_ago(1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with models.DAG(
+    dag_id='gdw_to_apmf_push_dag',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+    tags=['sts', 'push'],
+) as dag:
+
+    transfer_job = CloudDataTransferServiceCreateJobOperator(
+        task_id='start_gdw_to_apmf_push_transfer',
+        body={
+            "description": "Push STS job from GDW to APMF",
+            "status": "ENABLED",
+            "projectId": "sandbox-corp-gdw-sfr-cd8b",
+            "transferSpec": {
+                "gcsDataSource": {
+                    "bucketName": "gdw-sandbox-bucket-1"
+                },
+                "gcsDataSink": {
+                    "bucketName": "apmf-sandbox-bucket-2"
+                },
+                "objectConditions": {
+                    "includePrefix": ["sts/"],
+                },
+                "transferOptions": {
+                    "deleteObjectsFromSourceAfterTransfer": False,
+                    "overwriteObjectsAlreadyExistingInSink": True,
+                },
+            },
+            "schedule": {
+                "scheduleStartDate": {
+                    "year": 2025,
+                    "month": 7,
+                    "day": 8
+                },
+                "scheduleEndDate": {
+                    "year": 2030,
+                    "month": 12,
+                    "day": 31
+                },
+                "startTimeOfDay": {
+                    "hours": 1,
+                    "minutes": 0
+                }
+            }
+        }
+    )
