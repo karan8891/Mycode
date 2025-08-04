08f013ea89bb71e0fd8962d01c757207a728c036
karan8891 <55722488+karan8891@users.noreply.github.com>
2025-08-04 18:53:42 +0530

    Update Code.py

--- Event/Code.py
+++ Event/Code.py
@@ -1,5 +1,20 @@
-"notificationConfig": {
-    "pubsubTopic": f"projects/{PROJECT_ID}/topics/sts-completion-topic",
-    "eventTypes": ["TRANSFER_OPERATION_SUCCESS"],
-    "payloadFormat": "JSON"
+resource "google_pubsub_subscription" "push_subscriptions" {
+  for_each = {
+    for k, v in local.topics : k => v
+    if v.subscription_type == "push"
+  }
+
+  name  = each.value.subscription_name
+  topic = "projects/${var.project_id}/topics/${each.key}"
+
+  push_config {
+    push_endpoint = each.value.push_endpoint
+    oidc_token {
+      service_account_email = each.value.sa_email
+    }
+  }
+
+  ack_deadline_seconds         = 10
+  retain_acked_messages        = true
+  message_retention_duration   = "604800s"
 }
