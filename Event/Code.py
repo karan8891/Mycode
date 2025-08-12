7bdfcce0af0b5fd3bb1c72c11f7406a0696d0f30
karan8891 <55722488+karan8891@users.noreply.github.com>
2025-08-12 20:17:13 +0530

    Update Code.py

--- Event/Code.py
+++ Event/Code.py
@@ -1,5 +1,6 @@
+import re, uuid
 from datetime import datetime
-import uuid
 
-job_name = f"event-sts-job-{datetime.utcnow().strftime('%Y%m%dT%H%M%S%f')}-{uuid.uuid4().hex[:8]}"
+job_name = f"event_sts_job_{datetime.utcnow().strftime('%Y%m%dT%H%M%S')}_{uuid.uuid4().hex[:8]}"
+job_name = re.sub(r'[^a-zA-Z0-9_-]', '_', job_name)[:128]
 body["transferJob"]["name"] = job_name
